c36f9b2684d6ac887d52ad357b029f8b61f64767
khun <khun@skbkontur.ru>
2017-07-02 15:19:07 +0300

    simple skype bot on winforms

---
+++ skype_bot_win/skype_bot_win/Main.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace skype_bot_win
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(763, 394);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 418);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Skype_Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ skype_bot_win/skype_bot_win/Main.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SKYPE4COMLib;
+using System.IO;
+using System.Net;
+
+namespace skype_bot_win
+{
+    public partial class Main : Form
+    {
+        private Skype service;
+
+        public class CFileContent
+        {
+            public string send_to { get; set; }
+            public string to { get; set; }
+            public string message { get; set; }
+
+            public CFileContent(string data)
+            {
+                string[] arr = data.Split('|');
+                send_to = arr[0];
+                to = arr[1];
+                message = arr[2];
+            }
+
+        }
+
+
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            service = new Skype();
+            if (!service.Client.IsRunning)
+                service.Client.Start(true, true);
+            service.Attach(7,true);
+            service.MessageStatus += service_MessageStatus;
+            listBox1.Items.Add("Skype connected...");
+        }
+
+        private void service_MessageStatus(ChatMessage pMessage, TChatMessageStatus Status)
+        {
+            try
+            {
+                listBox1.Items.Add(String.Format("Date: {0},Chat: {1},Send from: {2}, status: {3}", DateTime.Now, pMessage.ChatName, pMessage.Sender.Handle, Status));
+                if (Status == TChatMessageStatus.cmsReceived)
+                {
+                    if (pMessage.Body.IndexOf("!") == 0)
+                    {
+
+                        // Remove trigger string and make lower case
+                        string command = pMessage.Body.Remove(0, 1).ToLower();
+
+                        // Send processed message back to skype chat window
+                        if (pMessage.ChatName.Contains('#'))
+                            service.Chat[pMessage.ChatName].SendMessage(ProcessCommand(command));
+                        else
+                            service.SendMessage(pMessage.Sender.Handle, ProcessCommand(command));
+                        listBox1.Items.Add(String.Format("Date: {0},Chat: {1},Send from: {2}, Message: {3}", DateTime.Now, pMessage.ChatName, pMessage.Sender.Handle, pMessage.Body));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter sw = new StreamWriter("process_log.txt", true))
+                    sw.WriteLine("{0}.Command: {1} - Error: {2}", DateTime.Now, pMessage.Body, ex.Message);
+            }
+
+        }
+
+
+        private string ProcessCommand(string str)
+        {
+                string result = null;
+                StringBuilder strb = new StringBuilder();
+                if (str.Equals("help"))
+                {
+                    strb.AppendLine("!help - current help.");
+                    strb.AppendLine("!bash - random bash quotes.");
+                    result = strb.ToString();
+                }
+                else if (str.Equals("bash"))
+                {
+                    WebClient wc = new WebClient();
+                    string bq = wc.DownloadString("http://bash.im/random");
+                    bq = bq.Substring(bq.IndexOf("div class=\"text\"") + 17);
+                    bq = bq.Substring(0, bq.IndexOf("</div>"));
+                    bq = bq.Replace("<br>", "\r\n").Replace("<br />", "\r\n").Replace("&lt;", "<").Replace("&gt;", ">").Replace("&quot;","\"");
+                    result = bq;
+                }
+               
+                return result;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\request";
+            string donePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\request\\done";
+            if (Directory.GetFiles(path).Length > 0)
+            {
+                foreach (string oneFile in Directory.GetFiles(path))
+                {
+                    try
+                    {
+                        CFileContent cont = new CFileContent(File.ReadAllText(oneFile));
+                        if (cont.send_to.Equals("person"))
+                            service.SendMessage(cont.to, cont.message);
+                        else if (cont.send_to.Equals("chat"))
+                            service.Chat[cont.to].SendMessage(cont.message);
+                        // moving
+                        FileInfo fi = new FileInfo(oneFile);
+                        File.Move(oneFile, donePath + "\\" + DateTime.Now.Ticks + "_" + fi.Name);
+                        listBox1.Items.Add(String.Format("Date: {0},Chat: {1},Send from: file, Message: {2}", DateTime.Now, cont.to, cont.message.Substring(0, 60)));
+                    }
+                    catch (Exception ex)
+                    {
+                        using (StreamWriter sw = new StreamWriter("log.txt", true))
+                            sw.WriteLine("{0} - Error:{1}", DateTime.Now, ex.Message);
+                    }
+                }
+            }
+        }
+
+    }
+}
